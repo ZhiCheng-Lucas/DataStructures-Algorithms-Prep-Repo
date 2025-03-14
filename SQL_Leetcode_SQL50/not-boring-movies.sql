4ffe05d42627270fd11ec8db2325e6219e676f7f
Zhi Cheng <89333157+ZhiCheng-Lucas@users.noreply.github.com>
2025-03-14 23:05:13 +0800

    Create not-boring-movies.sql

---
+++ SQL_Leetcode_SQL50/not-boring-movies.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT id, movie, description, rating
+FROM Cinema 
+WHERE 
+    description != 'boring'
+    AND 
+    id % 2 != 0
+ORDER BY rating DESC
