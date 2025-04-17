bb717a40dd55aed412c72a9093a45bad3617f043
Zhi Cheng <89333157+ZhiCheng-Lucas@users.noreply.github.com>
2025-04-17 09:13:36 +0800

    Create biggest-single-number.sql

---
+++ SQL_Leetcode_SQL50/biggest-single-number.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+WITH single_number AS
+    (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+    )
+
+SELECT MAX(num) AS num FROM single_number
